bc6eb693e4279aff92be4cc3aac1f4f1fad258a1
Jepmoltho <jept@itu.dk>
2021-12-08 18:26:50 +0100

    functional user registration

---
+++ src/UserRegistration.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import Parse from "parse";
+
+function UserRegistration() {
+  const [username, setUsername] = useState("");
+  const handleChangeUser = (e) => {
+    setUsername(e.target.value);
+  };
+  const [password, setPassword] = useState("");
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const doUserRegistration = async function () {
+    const usernameValue = username;
+    const passwordValue = password;
+    // Since the signUp method returns a Promise, we need to call it using await
+    return await Parse.User.signUp(usernameValue, passwordValue)
+      .then((createdUser) => {
+        alert(
+          "Succes! User " +
+            createdUser.getUsername() +
+            " was succesfully created!"
+        );
+        return true;
+      })
+      .catch((error) => {
+        alert("Error caught: ", error);
+        return false;
+      });
+  };
+
+  return (
+    <>
+      <input
+        style={styles.input}
+        value={username}
+        placeholder={"Username"}
+        onChange={handleChangeUser}
+        //onChangeText={(text) => setUsername(text)}
+      ></input>
+      <input
+        style={styles.input}
+        value={password}
+        placeholder={"Password"}
+        onChange={handleChangePassword}
+      ></input>
+      <br />
+      <button onClick={doUserRegistration}>Create user</button>
+    </>
+  );
+}
+
+const styles = {
+  input: {
+    margin: 50,
+  },
+};
+
+export default UserRegistration;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Parse from "parse";
+//import AsyncStorage from "@react-native-async-storage/async-storage";
+
+//Initializing the SDK.
+//Parse.setAsyncStorage(AsyncStorage);
+//You need to copy BOTH the the Application ID and the Javascript Key from: Dashboard->App Settings->Security & Keys
+
+Parse.initialize(
+  "DIZIpBQGZMyPSUqdca6GEfb5POe0sUfHNbtQ6Hzx",
+  "FmbiV3rcXgDGIBRbyAIoD3z6yYuxegPsWy3UITVh"
+);
+
+Parse.serverURL = "https://parseapi.back4app.com/";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
